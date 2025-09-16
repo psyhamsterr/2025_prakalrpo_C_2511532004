de0dfba8d9a93b5b900db5e6b03bffe0088e8e60
lenovo <lenovo@LAPTOP-J0SFHSHC>
2025-09-16 11:25:56 +0700

    praktikum pekan 2

---
+++ KelasKuliah/src/pekan2/DeklarasiVariable.java
@@ -0,0 +1,20 @@
+package pekan2;
+
+public class DeklarasiVariable {
+
+	static int umur=25;
+	public static void main(String[] args) {
+		int kode;
+		boolean isDIbawahUmur;
+		kode = 1234;
+		double gaji;
+		gaji = 5500000.23;
+		isDIbawahUmur = true;
+		System.out.println("status: "+isDIbawahUmur);
+		System.out.println("kode: "+ kode);
+		System.out.println("umur: "+ umur);
+		System.out.println("gaji: "+ gaji);
+
+	}
+
+}
---
+++ KelasKuliah/src/pekan2/KelilingLIngkaran.java
@@ -0,0 +1,13 @@
+package pekan2;
+
+public class KelilingLIngkaran {
+
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		double radius = 30;
+		
+		System.out.println("keliling =" + 2 * PI * radius);	
+
+	}
+
+}
