26f9ab28edc6093ef74a72aa211cb039ad900c7e
psyhamsterr <herojabrik@gmail.com>
2025-10-29 16:44:11 +0700

    praktikum pekan 5 2511532004

---
+++ prakalrpo_2025_C_2511532004/src/pekan5_2511532004/NestedFor2_2511532004.java
@@ -0,0 +1,15 @@
+package pekan5_2511532004;
+
+public class NestedFor2_2511532004 {
+	public static void main(String[] args) {
+		for (int i=0; i<=5; i++) {
+			for (int j=0; j<=5; j++) {
+			System.out.print(i + j +" ");	
+			} 
+		System.out.println();
+
+	}
+
+}
+
+}
---
+++ prakalrpo_2025_C_2511532004/src/pekan5_2511532004/PerulanganFor4_2511532004.java
@@ -0,0 +1,35 @@
+package pekan5_2511532004;
+
+import java.util.Scanner;
+
+public class PerulanganFor4_2511532004 {
+	public static void main(String[] args) {
+		int jumlah = 0;
+		int batas;
+		
+		Scanner input= new Scanner(System.in);
+		System.out.print("masukkan nilai batas = ");
+		batas = input.nextInt();
+		input.close();
+		
+		for (int i=1; i<=10; i++) {
+			System.out.println(i);
+			jumlah = jumlah + i;
+			if (i<batas) {
+				System.out.print(" + ");
+				
+			}
+			else {
+				System.out.print(" = ");
+			}
+			System.out.print(jumlah);
+			}
+			 }
+		
+		
+		
+		
+		
+	}
+
+
