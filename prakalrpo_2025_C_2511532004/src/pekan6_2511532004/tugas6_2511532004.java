03a0993494177b589ee55451167b3e9b692a468b
lenovo <lenovo@LAPTOP-J0SFHSHC>
2025-11-11 11:50:11 +0700

    pekan 7 praktikum alpro

--- prakalrpo_2025_C_2511532004/src/pekan6_2511532004/tugas6_2511532004.java
+++ prakalrpo_2025_C_2511532004/src/pekan6_2511532004/tugas6_2511532004.java
@@ -6,17 +6,15 @@ public class tugas6_2511532004 {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        while (sum != 7) {
+        int jumlah = 0;
+        while (jumlah != 7) {
             System.out.print("Apakah mau menggoncang dadu? (y/n): ");
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("y")) {
                 int dadu1 = rand.nextInt(6) + 1;
                 int dadu2 = rand.nextInt(6) + 1;
-                sum = dadu1 + dadu2;
-                System.out.println("Kamu dapat " + dadu1 + " dan " + dadu2 + " dengan total " + sum);
-            }else {
-            	System.out.println("program berhenti");
+                jumlah = dadu1 + dadu2;
+                System.out.println("Kamu dapat " + dadu1 + " dan " + dadu2 + " dengan total " + jumlah);
             }
         }
         scanner.close();
